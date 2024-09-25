91b828da7aec267e90f7e319ec3548a7c96fb51d
Ashir-Valjee <ashir@eco-motive.com>
2024-09-25 13:58:56 +0100

    put in new user form

--- Client/app.js
+++ Client/app.js
@@ -1,28 +1,38 @@
 console.log("Hello Everyone");
 
-document.getElementById("login-form").addEventListener('submit',async function (e){
-  e.preventDefault();
-  const username=document.getElementById("username-input").value;
-  const preferencesOutput = document.getElementById('preferencesOutput');
-  const bodyPreference = document.querySelector("body");
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username-input").value;
+    const preferencesOutput = document.getElementById("preferencesOutput");
+    const bodyPreference = document.querySelector("body");
 
- // Use fetch to send data to the server
- const response = await fetch('http://localhost:8080/get-login', { 
-  method: 'POST',  // Switch to POST because we're sending data
-  headers: {
-    'Content-Type': 'application/json', 
-  },
-  body: JSON.stringify({ username })  // Send the username as part of the request body in JSON format
-});
+    // Use fetch to send data to the server
+    const response = await fetch("http://localhost:8080/get-login", {
+      method: "POST", // Switch to POST because we're sending data
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username }), // Send the username as part of the request body in JSON format
+    });
 
-const data = await response.json();  // Await the response and parse the data as JSON
+    const data = await response.json(); // Await the response and parse the data as JSON
 
-// Update the page with the preferences object returned from the fetch
-preferencesOutput.textContent = `Preferences: ${data.preferences}`;
+    // Update the page with the preferences object returned from the fetch
+    preferencesOutput.textContent = `Preferences: ${data.preferences}`;
 
-bodyPreference.classList.add("change-body");
-console.log(bodyPreference);
-});
+    let currentPreference = data.preferences;
+
+    if (currentPreference == "light") {
+      bodyPreference.className = "change-body";
+    } else if (currentPreference == "dark") {
+      bodyPreference.className = "change-body-dark";
+    }
+
+    // document.body.style.backgroundColor = "blue";
+    console.log(bodyPreference);
+  });
 
 // assigning sounds to a const variable
 const happySound = new Audio("./assets/happy.mp3");
@@ -45,4 +55,3 @@ function handleAngrySound() {
 
 happyButton.addEventListener("click", handleHappySound);
 angryButton.addEventListener("click", handleAngrySound);
-
