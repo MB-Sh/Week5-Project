c20542c71c1b86a7984b3a83293f2b1945a0f236
Ashir-Valjee <ashir@eco-motive.com>
2024-09-26 10:07:06 +0100

    added new user form and styling changes

--- Client/app.js
+++ Client/app.js
@@ -1,12 +1,26 @@
 console.log("Hello Everyone");
+const bodyPreference = document.querySelector("main");
+const welcomeMessage = document.getElementById("welcome-message");
+
+function openForm() {
+  document.getElementById("login-form-section").style.display = "block";
+  document.getElementById("main-content").classList.add("blur");
+}
+
+function closeForm() {
+  document.getElementById("login-form-section").style.display = "none";
+  document.getElementById("main-content").classList.remove("blur");
+}
+
+window.onload = function () {
+  openForm();
+};
 
 document
   .getElementById("login-form")
   .addEventListener("submit", async function (e) {
     e.preventDefault();
     const username = document.getElementById("username-input").value;
-    const preferencesOutput = document.getElementById("preferencesOutput");
-    const bodyPreference = document.querySelector("body");
 
     // Use fetch to send data to the server
     const response = await fetch("http://localhost:8080/get-login", {
@@ -20,7 +34,6 @@ document
     const data = await response.json(); // Await the response and parse the data as JSON
 
     // Update the page with the preferences object returned from the fetch
-    preferencesOutput.textContent = `Preferences: ${data.preferences}`;
 
     let currentPreference = data.preferences;
 
@@ -30,21 +43,40 @@ document
       bodyPreference.className = "change-body-dark";
     }
 
+    welcomeMessage.textContent = `Welcome ${username}`;
+    console.log(welcomeMessage);
     // document.body.style.backgroundColor = "blue";
     console.log(bodyPreference);
+    document.getElementById("login-form").reset();
+    closeForm();
   });
 
 // assigning sounds to a const variable
-const happySound = new Audio("./assets/happy.mp3");
-const angrySound = new Audio("./assets/angry.mp3");
+const happySound = new Audio("./assets/sound1.wav");
+const calmSound = new Audio("./assets/sound2.wav");
+const neutralSound = new Audio("./assets/sound3.wav");
+const sadSound = new Audio("./assets/sound4.wav");
+const angrySound = new Audio("./assets/sound5.wav");
 
 // get button elements
 const happyButton = document.getElementById("happy-button");
+const calmButton = document.getElementById("calm-button");
+const neutralButton = document.getElementById("neutral-button");
+const sadButton = document.getElementById("sad-button");
 const angryButton = document.getElementById("angry-button");
 
 // create event handlers for our buttons
 function handleHappySound() {
   happySound.play();
+}
+function handleCalmSound() {
+  calmSound.play();
+}
+function handleNeutralSound() {
+  neutralSound.play();
+}
+function handleSadSound() {
+  sadSound.play();
 }
 
 function handleAngrySound() {
@@ -54,4 +86,28 @@ function handleAngrySound() {
 // event listeners for our button
 
 happyButton.addEventListener("click", handleHappySound);
+calmButton.addEventListener("click", handleCalmSound);
+neutralButton.addEventListener("click", handleNeutralSound);
+sadButton.addEventListener("click", handleSadSound);
 angryButton.addEventListener("click", handleAngrySound);
+
+const newUserForm = document.getElementById("new-user-form");
+//Event handler for new user submit button
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(newUserForm);
+  const formValues = Object.fromEntries(formData);
+  console.log(formValues);
+
+  fetch("http://localhost:8080/new-user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+
+  newUserForm.reset();
+}
+newUserForm.addEventListener("submit", handleSubmit);
--- server/seed.js
+++ server/seed.js
@@ -3,12 +3,12 @@ import { db } from "./server.js";
 // if we already have a table with the same name we need to make sure
 // we dont create a new table with the same name
 
-// db.query(`CREATE TABLE IF NOT EXISTS preferences (
-//   id SERIAL PRIMARY KEY,
-//   username TEXT NOT NULL,
-//   preferences VARCHAR(100)
+db.query(`CREATE TABLE IF NOT EXISTS preferences (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  preferences VARCHAR(100)
 
-// );`);
+);`);
 
 // db.query(`INSERT INTO preferences (username,preferences)
 //   VALUES ('Yusef','dark')`);
